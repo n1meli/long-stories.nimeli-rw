d11ad84b1cd3c465c4889c5ca16eb53599865a71
n1meli <ddddmhg@gmail.com>
2025-07-13 23:25:51 +0300

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: '50px', textAlign: 'center' }}>
+      <h1>Hello from Vercel 🎉</h1>
+      <p>Now your project is working!</p>
+    </div>
+  );
+}
