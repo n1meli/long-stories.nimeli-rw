7c106a55c7819ff2ac7787bc46b3b12dcb843823
n1meli <ddddmhg@gmail.com>
2025-07-14 19:36:29 +0300

    Delete pages directory

--- pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ textAlign: 'center', paddingTop: '50px' }}>
-      <h1>Welcome to Next.js App!</h1>
-      <p>This is a clean starter project deployed with Vercel.</p>
-    </div>
-  )
-}
